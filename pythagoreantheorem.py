ac99e49be7db3b59eda930584e590f598ec5e3a0
Robbiesactforthehub <03robert.strongmorse@kamsc.org>
2025-09-19 12:24:32 -0400

    Update pythagoreantheorem.py

--- pythagoreantheorem.py
+++ pythagoreantheorem.py
@@ -2,7 +2,7 @@ class Student:
     def __init__(self,A,B):
         self.A=A
         self.B=B
-    def get_name(self):
+    def hypotenuse(self):
         return math.sqrt(self.A**2+self.B**2)
     
     
